e63cd549fda279c641929134b2a31138e647731a
reydevega1 <reydevega@gmail.com>
2017-01-31 13:23:28 -0800

    Coding Object-Oriented Cobol-DefineClass
    Defining a class in cobol to use for OO.

---
+++ Coding Object-Oriented Cobol-DefineClass.cbl
@@ -0,0 +1,18 @@
+      ******************************************************************
+      * Author:rey devega
+      * Date:1/31/2017
+      * Purpose:Coding Object-Oriented COBOL - Define CLASS
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       Class-ID. elpsortTbl    Inherits Base.
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       REPOSITORY
+           Class Base IS 'Base'.
+       Factory.
+           End Factory.
+
+           Object.
+               END Object.
+       End Class elpSortTbl.
